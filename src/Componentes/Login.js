9b716d4deb71b4da5113cdc4290f8f957794b432
liamkuny <117608651+liamkuny@users.noreply.github.com>
2023-11-25 21:29:50 -0300

    commit

--- src/Componentes/Login.js
+++ src/Componentes/Login.js
@@ -1,96 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { View, TextInput, Button, Text, StyleSheet, SafeAreaView } from "react-native";
+import axios from "axios";
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleLogin = async () => {
-    if (!email || !password) {
-      alert('Por favor, completa todos los campos.');
+  const handleSubmit = () => {
+    setLoading(true);
+
+    if (email === "" || password === "") {
+      setError("Los campos de email y contraseña son obligatorios.");
+      setLoading(false);
       return;
     }
 
-    try {
-      setLoading(true);
-
-      const response = await fetch('http://challenge-react.alkemy.org/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
+    
+      axios.post("http://challenge-react.alkemy.org/", {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          setLoading(false);
+          
+          navigation.navigate("Menu"); 
+        } else {
+          setLoading(false);
+          setError(res.data.message);
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
       });
-
-      if (!response.ok) {
-        throw new Error('Error en la autenticación');
-      }
-
-      const data = await response.json();
-
-      // Almacena el token en el contexto o donde sea necesario
-      console.log('Token:', data.token);
-
-      // Redirige al Home (puedes utilizar React Router para esto)
-    } catch (error) {
-      alert('Error al intentar iniciar sesión. Por favor, verifica tus credenciales.');
-    } finally {
-      setLoading(false);
-    }
   };
 
   return (
-    <div
-      style={{
-        maxWidth: '400px',
-        margin: 'auto',
-        padding: '20px',
-        border: '1px solid #ccc',
-        borderRadius: '5px',
-        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-        textAlign: 'center',
-      }}
-    >
-      <h1>Formulario de Login</h1>
-      <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <label style={{ margin: '10px 0' }}>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={{ padding: '8px', marginTop: '5px' }}
-          />
-        </label>
-        <br />
-        <label style={{ margin: '10px 0' }}>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{ padding: '8px', marginTop: '5px' }}
-          />
-        </label>
-        <br />
-        <button
-          onClick={handleLogin}
-          disabled={loading}
-          style={{
-            padding: '10px',
-            marginTop: '10px',
-            backgroundColor: loading ? '#ccc' : '#4caf50',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '4px',
-            cursor: loading ? 'not-allowed' : 'pointer',
-          }}
-        >
-          {loading ? 'Procesando...' : 'Enviar'}
-        </button>
-      </form>
-    </div>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Contraseña"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button
+        title="Iniciar Sesión"
+        onPress={handleSubmit}
+        disabled={loading}
+      />
+      {error && <Text style={styles.error}>{error}</Text>}
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: "80%",
+  },
+  error: {
+    color: "red",
+    marginTop: 10,
+  },
+});
 
 export default Login;
