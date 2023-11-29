261016b9039d62bc247e300308e6879341b92383
Liam Kuniewsky <liamkuniewsky@gmail.com>
2023-11-29 12:40:56 -0300

    commit

--- src/Componentes/Busqueda.js
+++ src/Componentes/Busqueda.js
@@ -1,4 +1,89 @@
-const Busqueda = ({}) => {
-   
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet } from "react-native";
+import axios from "axios";
+
+const Busqueda = ({ navigation, route }) => {
+  const [filtro, setFiltro] = useState("");
+  const [resultados, setResultados] = useState([]);
+
+  const buscarPlatos = () => {
+    if (filtro.length > 2) {
+      axios
+        .get("https://api.spoonacular.com/recipes/complexSearch", {
+          params: {
+            apiKey: "1fcffc9826e745be90f1f569128f1a5c", 
+            query: filtro,
+          },
+        })
+        .then((response) => {
+          setResultados(response.data.results);
+        })
+        .catch((error) => {
+          console.error("Error al buscar platos:", error);
+        });
+    } else {
+      setResultados([]);
+    }
+  };
+
+  const agregarAlMenu = (plato) => { 
+    route.params.agregarAlMenu(plato);
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Buscar Platos</Text>
+      <TextInput
+        placeholder="Ingrese el nombre del plato"
+        value={filtro}
+        onChangeText={(text) => setFiltro(text)}
+        style={styles.input}
+      />
+      <Button title="Buscar" onPress={buscarPlatos} />
+      <FlatList
+        data={resultados}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => agregarAlMenu(item)}>
+            <View style={styles.resultadoItem}>
+              <Text>{item.title}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 };
-  export default Busqueda;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 15,
+    width: "80%",
+    fontSize: 16,
+  },
+  resultadoItem: {
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    width: "80%",
+  },
+});
+
+export default Busqueda;
--- src/Componentes/Menu.js
+++ src/Componentes/Menu.js
@@ -1,14 +1,116 @@
-// En Menu.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, TouchableOpacity, Button, StyleSheet } from "react-native";
 
-const Menu = () => {
-  // ... tu código ...
+const Menu = ({ navigation }) => {
+  const [menu, setMenu] = useState([]);
+  const [precioAcumulado, setPrecioAcumulado] = useState(0);
+  const [healthScorePromedio, setHealthScorePromedio] = useState(0);
+
+  useEffect(() => {
+    actualizarEstadisticas();
+  }, [menu]);
+
+  const actualizarEstadisticas = () => {
+    let acumulado = 0;
+    let totalHealthScore = 0;
+
+    menu.forEach((plato) => {
+      acumulado += plato.precio || 0;
+      totalHealthScore += plato.healthScore || 0;
+    });
+
+    const promedio = menu.length > 0 ? totalHealthScore / menu.length : 0;
+
+    setPrecioAcumulado(acumulado);
+    setHealthScorePromedio(promedio);
+  };
+
+  const eliminarDelMenu = (platoId) => {
+    const nuevoMenu = menu.filter((plato) => plato.id !== platoId);
+    setMenu(nuevoMenu);
+  };
+
+  const verDetalle = (plato) => {
+    navigation.navigate("DetallePlato", { plato });
+  };
+
+  const irABusqueda = () => {
+    navigation.navigate("Busqueda", { agregarAlMenu: agregarAlMenu });
+  };
+
+  const agregarAlMenu = (plato) => {
+    if (menu.length < 4 && (plato.vegano ? contarVeganos() < 2 : contarNoVeganos() < 2)) {
+      setMenu([...menu, plato]);
+    } else {
+      alert("No se puede agregar más platos al menú.");
+    }
+  };
+
+  const contarVeganos = () => {
+    return menu.filter((plato) => plato.vegano).length;
+  };
+
+  const contarNoVeganos = () => {
+    return menu.filter((plato) => !plato.vegano).length;
+  };
+
   return (
-    <View>
-      <Text>Contenido del menú</Text>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Menú:</Text>
+      <Text style={styles.info}>Precio Acumulado: ${precioAcumulado}</Text>
+      <Text style={styles.info}>Promedio de HealthScore: {healthScorePromedio.toFixed(2)}</Text>
+      <FlatList
+        data={menu}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => verDetalle(item)}>
+            <View style={styles.platoItem}>
+              <Text>{item.nombre}</Text>
+              <TouchableOpacity onPress={() => eliminarDelMenu(item.id)}>
+                <Text style={styles.eliminarTexto}>Eliminar del Menú</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+      <Button title="Ir a Búsqueda" onPress={irABusqueda} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  info:
+  {
+    
+    fontSize: 15,
+    width: 225,
+    fontWeight: "bold",
+    
+  },
+  platoItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    width: "80%",
+  },
+  eliminarTexto: {
+    color: "red",
+  },
+});
 
 export default Menu;
